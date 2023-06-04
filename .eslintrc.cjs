10e83d3723c2f639b1cc87b46b5f15abda1b748b
Mystist <gjl87910lq@gmail.com>
2023-06-04 21:09:13 +0800

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh', 'simple-import-sort'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react-refresh/only-export-components': 'warn',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
+}
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './lib'
+
+function App() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <p className="text-purple-500">react-chatgpt</p>
+    </div>
+  )
+}
+
+export default App
