4fd8173605e0a9d076e17f9ab946e44c014d94d2
Mystist <gjl87910lq@gmail.com>
2023-06-05 11:01:28 +0800

    feat: add id as tailwind scope

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { ReactChatGPT } from './lib'
 
 function App() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center space-y-4">
-      <p className="text-purple-500">react-chatgpt</p>
+    <div id="react-chatgpt">
+      <div className="flex min-h-screen flex-col items-center justify-center space-y-4">
+        <p className="text-purple-500">react-chatgpt</p>
 
-      <ReactChatGPT />
+        <ReactChatGPT />
+      </div>
     </div>
   )
 }
--- src/lib/components/Greeting/index.tsx
+++ src/lib/components/Greeting/index.tsx
@@ -6,13 +6,13 @@ export default function Index() {
   }, [])
 
   return (
-    <>
+    <div id="react-chatgpt">
       <button
         className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         onClick={onClick}
       >
         Greeting
       </button>
-    </>
+    </div>
   )
 }
