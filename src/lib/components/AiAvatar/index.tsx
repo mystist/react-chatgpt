06c0afe1b588031fa31606e5b2555d671180d8f6
Mystist <gjl87910lq@gmail.com>
2023-06-12 18:42:45 +0800

    feat: remove avatar video

--- src/lib/components/AiAvatar/index.tsx
+++ src/lib/components/AiAvatar/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from 'react'
 
 import { baseUrl } from '../../requests'
-import { classNames, getIdentifier } from '../../utils'
+import { getIdentifier } from '../../utils'
 
 export default function Index({ whisperUuid, nowPlayingWhisperUuidState }: any) {
   const identifier = getIdentifier()
@@ -28,9 +28,6 @@ export default function Index({ whisperUuid, nowPlayingWhisperUuidState }: any) 
       {identifier && (
         <div className="relative flex h-12 w-12 flex-col overflow-hidden rounded-full">
           <img src={`${baseUrl}/uploads/placeholder/images/avatar-${identifier}.png`} alt="avatar" className="h-12 w-12 scale-105 rounded-full bg-[#bfbdbe]" />
-          <video className={classNames(isPlaying ? 'block' : 'hidden', 'absolute h-full w-full')} ref={elemRef} muted={true} loop={true}>
-            <source src={`${baseUrl}/uploads/placeholder/videos/placeholder-talk-${identifier}.mp4`} type="video/mp4" />
-          </video>
         </div>
       )}
     </>
