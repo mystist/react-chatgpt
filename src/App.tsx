14b81f963ad61193bff1cd2ffedde45db046c3e9
Mystist <gjl87910lq@gmail.com>
2023-06-25 10:56:33 +0800

    chore: tweak

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
         </div>
       </div>
 
-      <ReactChatGPT status={status} setStatus={setStatus} identifier={import.meta.env.VITE_IDENTIFIER} lang="ja" />
+      <ReactChatGPT status={status} setStatus={setStatus} identifier={import.meta.env.VITE_IDENTIFIER} lang={import.meta.env.VITE_LANG} />
     </div>
   )
 }
