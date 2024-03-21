c0e774591a069697c70b762e588950c0e8d5cc1a
gongzhaoman <gongzhaoman@outlook.com>
2024-03-21 10:30:13 +0800

    feat: use default color for avatar

--- src/lib/components/AiAvatar/index.tsx
+++ src/lib/components/AiAvatar/index.tsx
@@ -28,7 +28,7 @@ export default function Index({ whisperUuid, nowPlayingWhisperUuidState }: any) 
       {agentName && (
         <div className="relative flex h-12 w-12 flex-col overflow-hidden rounded-full">
           {!avatarPath || avatarPath === 'auto' ? (
-            <Avatar size={48} name={agentName} colors={['#59b6b9', '#63af56', '#f0d457', '#dd883a', '#d53932']} />
+            <Avatar size={48} name={agentName} />
           ) : (
             <a href={avatarPath} target="_blank">
               <img src={avatarPath} alt="avatar" className="h-12 w-12 rounded-full bg-[#bfbdbe]" />
